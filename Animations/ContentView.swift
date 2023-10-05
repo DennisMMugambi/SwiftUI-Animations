1d4f6ed5b6dbbb76e129a04eb7a889a2dd6aa8c6
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-05 15:32:40 +0300

    create a spring loaded 3d rotation effect animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     var body: some View {
                 Button("Tap Me") {
-                    withAnimation {
+                    withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
                         animationAmount += 360
                     }
                 }
