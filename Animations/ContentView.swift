9e0b8556f1068cf1bfc64b73ee35989affcfef2a
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-05 15:23:10 +0300

    adds 3d rotating (explicit) animation effect

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,26 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
-        Button("Tap Me") {
-          // animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
-        )
-        .onAppear{
-            animationAmount = 2
-        }
+                Button("Tap Me") {
+                    withAnimation {
+                        animationAmount += 360
+                    }
+                }
+                .padding(50)
+                .background(.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y:1, z:1))
     }
 }
 
