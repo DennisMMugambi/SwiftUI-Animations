4a0439dab96c488b665be8082ab5beab26b52253
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-05 12:51:53 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Dennis Mutwiri Mugambi on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
