83d1251bfa6909d027cd78ee623dd093a415f896
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-06 08:52:53 +0300

    adds drag gesture on card

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,19 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 0.0
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-                Button("Tap Me") {
-                    withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                        animationAmount += 360
-                    }
-                }
-                .padding(50)
-                .background(.red)
-                .foregroundColor(.white)
-                .clipShape(Circle())
-                .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y:1, z:1))
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged{ dragAmount = $0.translation }
+                    .onEnded{ _ in dragAmount = .zero }
+            )
     }
 }
 
